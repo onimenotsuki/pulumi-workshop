fce4c5f3f3bdad893492678591d7e8ea3e811327
Edgar Talledos Robledos <onimenotsuki@gmail.com>
2023-04-11 17:26:42 -0600

    WIP

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket('pulumi-workshop-dev-edgar-talledos');
+const bucket = new aws.s3.Bucket('pulumi-workshop-dev-edgar-talledos-s3-bucket');
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
